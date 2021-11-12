89492bb5951629cdb58fa981fcaf994f3f1c75e6
adisekar <adithya.kiran.sekar@gmail.com>
2021-11-12 16:28:38 -0500

    Code and test scoop options images from server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,4 +9,11 @@ export const handlers = [
             ])
         )
     }),
+    rest.get('*', (req, res, ctx) => {
+        console.error(`Please add request handler for ${req.url.toString()}`);
+        return res(
+            ctx.status(500),
+            ctx.json({ error: "Please add request handler" })
+        )
+    })
 ]
