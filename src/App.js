886251944f4142b2bf974124f086d1d631698683
adisekar <adithya.kiran.sekar@gmail.com>
2021-11-12 18:24:46 -0500

    Add Sundae server and order entry error scenarios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import OrderEntry from './pages/entry/OrderEntry';
 import SummaryForm from './pages/summary/SummaryForm';
 
 function App() {
   return (
     <div className="App">
+      <OrderEntry />
       <SummaryForm />
     </div>
   );
