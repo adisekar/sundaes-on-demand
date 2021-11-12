6a1f766b7b22db37219bf1ceff4d5cc44e10a47c
adisekar <adithya.kiran.sekar@gmail.com>
2021-11-12 17:29:11 -0500

    Add topping options code and test

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,6 +9,15 @@ export const handlers = [
             ])
         )
     }),
+    rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
+        return res(
+            ctx.json([
+                { name: 'Cherries', imagePath: '/images/cherries.png' },
+                { name: 'M&Ms', imagePath: '/images/m-and-ms.png' },
+                { name: 'Hot fudge', imagePath: '/images/hot-fudge.png' },
+            ])
+        );
+    }),
     rest.get('*', (req, res, ctx) => {
         console.error(`Please add request handler for ${req.url.toString()}`);
         return res(
