63f0b0b22541d506cda714059afda92bde3a4ecc
adisekar <adithya.kiran.sekar@gmail.com>
2021-11-15 13:20:37 -0500

    Code and Test for Scoops subtotal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import Container from 'react-bootstrap/Container';
 import OrderEntry from './pages/entry/OrderEntry';
-import SummaryForm from './pages/summary/SummaryForm';
+import { OrderDetailsProvider } from './contexts/OrderDetails';
 
 function App() {
   return (
-    <div className="App">
-      <OrderEntry />
-      <SummaryForm />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider */}
+    </Container>
   );
 }
 
